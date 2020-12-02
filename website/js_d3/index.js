0839983073e7c38dd9a97b8499817d7658455a5c
sofiamoniz <sofiamoniz@ua.pt>
2020-12-02 10:34:01 +0000

    years doughnut

--- website/js_d3/index.js
+++ website/js_d3/index.js
@@ -104,14 +104,24 @@ d3.csv("/data/data.csv", function(data){
    .attr("dy", 5)
    
    ////////////////////////////////////////////Genres//////////////////////////////////////////////////////////////
-   var sorted = [];
-   for(var key in dictGenres) {
-      sorted[sorted.length] = key;
-   }
-   data = {}
-   sorted.slice(0,10).forEach(function(g) {
-      data[g] = dictGenres[g]      
+   
+   // Create items array
+   var items = Object.keys(dictGenres).map(function(key) {
+      return [key, dictGenres[key]];
    });
+   
+   // Sort the array based on the second element
+   items.sort(function(first, second) {
+      return second[1] - first[1];
+   });
+   
+   data={}
+   // Create a new array with only the first 5 items
+   //console.log(items.slice(0, 10));
+   items.slice(0, 10).forEach(function(g) {
+      data[g[0]]=g[1]
+   });
+
    // set the dimensions and margins of the graph
    var width = 450
    height = 450
@@ -186,7 +196,7 @@ d3.csv("/data/data.csv", function(data){
    .enter()
    .append('text')
    .text( function(d) {return d.data.key } )
-   .style("font-size", 9)
+   .style("font-size", 7)
    .style("font-weight", "bold")
    .attr('transform', function(d) {
       var pos = outerArc.centroid(d);
@@ -200,5 +210,107 @@ d3.csv("/data/data.csv", function(data){
    })
 
    ////////////////////////////////////////////Years//////////////////////////////////////////////////////////////
+    // Create items array
+   var items = Object.keys(dictYears).map(function(key) {
+      return [key, dictYears[key]];
+   });
+   
+   // Sort the array based on the second element
+   items.sort(function(first, second) {
+      return second[1] - first[1];
+   });
+   
+   data={}
+   // Create a new array with only the first 5 items
+   //console.log(items.slice(0, 10));
+   items.slice(0, 10).forEach(function(g) {
+      data[g[0]]=g[1]
+   });
+   // set the dimensions and margins of the graph
+   var width = 450
+   height = 450
+   margin = 40
+   // The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+   var radius = Math.min(width, height) / 2 - margin
 
+   // append the svg object to the div called 'my_dataviz'
+   var svg = d3.select("#years")
+   .append("svg")
+   .attr("width", width)
+   .attr("height", height)
+   .append("g")
+   .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+
+   // set the color scale
+   var color = d3.scaleOrdinal()
+   .domain(Object.keys(data))
+   .range(d3.schemeDark2);
+
+   // Compute the position of each group on the pie:
+   var pie = d3.pie()
+   .sort(null) // Do not sort group by size
+   .value(function(d) {return d.value; })
+   var data_ready = pie(d3.entries(data))
+
+   // The arc generator
+   var arc = d3.arc()
+   .innerRadius(radius * 0.5)         // This is the size of the donut hole
+   .outerRadius(radius * 0.8)
+
+   // Another arc that won't be drawn. Just for labels positioning
+   var outerArc = d3.arc()
+   .innerRadius(radius * 0.9)
+   .outerRadius(radius * 0.9)
+
+   // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+   svg
+   .selectAll('allSlices')
+   .data(data_ready)
+   .enter()
+   .append('path')
+   .attr('d', arc)
+   .attr('fill', function(d){ return(color(d.data.key)) })
+   .attr("stroke", "white")
+   .style("stroke-width", "2px")
+   .style("opacity", 0.7)
+
+   // Add the polylines between chart and labels:
+   svg
+   .selectAll('allPolylines')
+   .data(data_ready)
+   .enter()
+   .append('polyline')
+   .attr("stroke", "black")
+   .style("fill", "none")
+   .attr("stroke-width", 1)
+   .attr('points', function(d) {
+   var posA = arc.centroid(d) // line insertion in the slice
+   var posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+   var posC = outerArc.centroid(d); // Label position = almost the same as posB
+   var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+   posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+   return [posA, posB, posC]
+   })
+
+   // Add the polylines between chart and labels:
+   svg
+   .selectAll('allLabels')
+   .data(data_ready)
+   .enter()
+   .append('text')
+   .text( function(d) {return d.data.key } )
+   .style("font-size", 10)
+   .style("font-weight", "bold")
+   .attr('transform', function(d) {
+      var pos = outerArc.centroid(d);
+      var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+      pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+      return 'translate(' + pos + ')';
+   })
+   .style('text-anchor', function(d) {
+      var midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+      return (midangle < Math.PI ? 'start' : 'end')
+   })
+   
 });
