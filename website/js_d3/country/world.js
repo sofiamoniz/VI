c32374a460efdd02fae739eab2ff5e97101444b3
sofiamoniz <sofiamoniz@ua.pt>
2020-12-06 15:52:02 +0000

    mouseover map

--- website/js_d3/country/world.js
+++ website/js_d3/country/world.js
@@ -38,7 +38,7 @@ d3.csv("/data/data.csv", function(data){
                     if (d.name == k){
                         d.longitude=+d.longitude
                         d.latitude=+d.latitude
-                        country_coord.push({val: dictCountriesNetflix[k], long: d.longitude,lat: d.latitude})
+                        country_coord.push({country:k, val: dictCountriesNetflix[k], long: d.longitude,lat: d.latitude})
                     }
                 });
             }
@@ -58,28 +58,65 @@ d3.csv("/data/data.csv", function(data){
             // Add a svg layer to the map
             L.svg().addTo(map);    
 
-            
+            var Tooltip = d3.select("#netflixCountry")
+            .append("div")
+            .attr("class", "tooltip")
+            .style("opacity", 1)
+            .style("background-color", "white")
+            .style("border", "solid")
+            .style("border-width", "2px")
+            .style("border-radius", "5px")
+            .style("padding", "5px")
+
             // Select the svg area and add circles:
             d3.select("#netflixCountry")
             .select("svg")
-            .selectAll("myCircles")
-            .data(country_coord)
-            .enter()
+                .selectAll("myCircles")
+                .data(country_coord)
+                .enter()
             .append("circle")
-            .attr("cx", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).x })
-            .attr("cy", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).y })
-            .attr("r", 14)
-            .style("fill", "#fc301e")
-            .attr("stroke", "#fc301e")
-            .attr("stroke-width", 3)
-            .attr("fill-opacity", .4)
-            
+                .attr("cx", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).x })
+                .attr("cy", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).y })
+                .attr("r", 14)
+                .style("fill", "#fc301e")
+                .attr("stroke", "#fc301e")
+                .attr("stroke-width", 3)
+                .attr("fill-opacity", .4)
+                .attr("pointer-events","visible")
+                .on("mouseover", function(d) {
+                    Tooltip.style("opacity", 1)
+                })
+                .on("mousemove", function(d) {
+                    Tooltip
+                      .html("Country: "+ d.country + "<br>" + "nº movies: " + d.val)
+                      .style("left", (d3.mouse(this)[0]+10) + "px")
+                      .style("top", (d3.mouse(this)[1]) + "px")
+                })
+                .on("mouseleave", function(d) {
+                    Tooltip.style("opacity", 0)
+                  });             
+                
+
 
             // Function that update circle position if something change
             function update() {
             d3.selectAll("circle")
             .attr("cx", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).x })
             .attr("cy", function(d){ return map.latLngToLayerPoint([d.lat, d.long]).y })
+            .attr("pointer-events","visible")
+            .on("mouseover", function(d) {
+                Tooltip.style("opacity", 1)
+            })
+            .on("mousemove", function(d) {
+                Tooltip
+                    .html("Country: "+ d.country + "<br>" + "nº movies: " + d.val)
+                    .style("left", (d3.mouse(this)+10) + "px")
+                    .style("top", (d3.mouse(this)+10) + "px")
+            })
+            .on("mouseleave", function(d) {
+                Tooltip.style("opacity", 0)
+                })             
+            
             }
 
             // If the user change the map (zoom or drag), I update circle position:
