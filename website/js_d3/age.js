2afb30e1ca20a7e7421d1f26fd144b84bdccae4f
sofiamoniz <sofiamoniz@ua.pt>
2020-12-09 15:54:41 +0000

    age comparasion done

--- website/js_d3/age.js
+++ website/js_d3/age.js
@@ -67,10 +67,15 @@ d3.csv("/data/data.csv", function(data){
     Object.keys(dictAgeNetflix).sort().forEach(function(key) {
         ordered[key] = dictAgeNetflix[key];
     });
-    age_value = []
+    age_value_netflix = []
+    all_platforms = {}
+    netflix_ages = []
     for(let k in ordered){
-        age_value.push({Age: k, Value: ordered[k]})
+        age_value_netflix.push({Age: k, Value: ordered[k]})
+        netflix_ages.push({age:k, value: ordered[k]})
+        
     } 
+    all_platforms["netflix"] = netflix_ages
 
     // set the dimensions and margins of the graph
     var margin = {top: 30, right: 30, bottom: 70, left: 60},
@@ -89,7 +94,7 @@ d3.csv("/data/data.csv", function(data){
     // X axis
     var x = d3.scaleBand()
     .range([ 0, width ])
-    .domain(age_value.map(function(d) { return d.Age; }))
+    .domain(age_value_netflix.map(function(d) { return d.Age; }))
     .padding(0.2);
     svg.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -117,7 +122,7 @@ d3.csv("/data/data.csv", function(data){
 
     // Bars
     svg.selectAll("mybar")
-    .data(age_value)
+    .data(age_value_netflix)
     .enter()
     .append("rect")
         .attr("x", function(d) { return x(d.Age); })
@@ -135,10 +140,13 @@ d3.csv("/data/data.csv", function(data){
     Object.keys(dictAgePrime).sort().forEach(function(key) {
         ordered[key] = dictAgePrime[key];
     });
-    age_value = []
+    age_value_prime = []
+    prime_ages = []
     for(let k in ordered){
-        age_value.push({Age: k, Value: ordered[k]})
+        age_value_prime.push({Age: k, Value: ordered[k]})
+        prime_ages.push({age:k, value: ordered[k]})
     } 
+    all_platforms["prime"]=prime_ages
 
     // set the dimensions and margins of the graph
     var margin = {top: 30, right: 30, bottom: 70, left: 60},
@@ -157,7 +165,7 @@ d3.csv("/data/data.csv", function(data){
     // X axis
     var x = d3.scaleBand()
     .range([ 0, width ])
-    .domain(age_value.map(function(d) { return d.Age; }))
+    .domain(age_value_prime.map(function(d) { return d.Age; }))
     .padding(0.2);
     svg.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -185,7 +193,7 @@ d3.csv("/data/data.csv", function(data){
 
     // Bars
     svg.selectAll("mybar")
-    .data(age_value)
+    .data(age_value_prime)
     .enter()
     .append("rect")
         .attr("x", function(d) { return x(d.Age); })
@@ -203,11 +211,14 @@ d3.csv("/data/data.csv", function(data){
     Object.keys(dictAgeHulu).sort().forEach(function(key) {
         ordered[key] = dictAgeHulu[key];
     });
-    age_value = []
+    age_value_hulu = []
+    hulu_ages = []
     for(let k in ordered){
-        age_value.push({Age: k, Value: ordered[k]})
+        age_value_hulu.push({Age: k, Value: ordered[k]})
+        hulu_ages.push({age:k, value: ordered[k]})
     } 
-
+    all_platforms["hulu"] = hulu_ages
+    console.log(all_platforms)
     // set the dimensions and margins of the graph
     var margin = {top: 30, right: 30, bottom: 70, left: 60},
         width = 460 - margin.left - margin.right,
@@ -225,7 +236,7 @@ d3.csv("/data/data.csv", function(data){
     // X axis
     var x = d3.scaleBand()
     .range([ 0, width ])
-    .domain(age_value.map(function(d) { return d.Age; }))
+    .domain(age_value_hulu.map(function(d) { return d.Age; }))
     .padding(0.2);
     svg.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -253,7 +264,7 @@ d3.csv("/data/data.csv", function(data){
 
     // Bars
     svg.selectAll("mybar")
-    .data(age_value)
+    .data(age_value_hulu)
     .enter()
     .append("rect")
         .attr("x", function(d) { return x(d.Age); })
@@ -271,10 +282,14 @@ d3.csv("/data/data.csv", function(data){
     Object.keys(dictAgeDisney).sort().forEach(function(key) {
         ordered[key] = dictAgeDisney[key];
     });
-    age_value = []
+    age_value_disney = []
+    disney_ages=[]
     for(let k in ordered){
-        age_value.push({Age: k, Value: ordered[k]})
+        age_value_disney.push({Age: k, Value: ordered[k]})
+        //all.push({platform: "disney", age:k, value: ordered[k]})
+        disney_ages.push({age:k, value: ordered[k]})
     } 
+    all_platforms["disney"]=disney_ages
 
     // set the dimensions and margins of the graph
     var margin = {top: 30, right: 30, bottom: 70, left: 60},
@@ -293,7 +308,7 @@ d3.csv("/data/data.csv", function(data){
     // X axis
     var x = d3.scaleBand()
     .range([ 0, width ])
-    .domain(age_value.map(function(d) { return d.Age; }))
+    .domain(age_value_disney.map(function(d) { return d.Age; }))
     .padding(0.2);
     svg.append("g")
     .attr("transform", "translate(0," + height + ")")
@@ -321,7 +336,7 @@ d3.csv("/data/data.csv", function(data){
 
     // Bars
     svg.selectAll("mybar")
-    .data(age_value)
+    .data(age_value_disney)
     .enter()
     .append("rect")
         .attr("x", function(d) { return x(d.Age); })
@@ -332,5 +347,199 @@ d3.csv("/data/data.csv", function(data){
         .on("mouseover", function(d){tooltip.text(d.Value); return tooltip.style("visibility", "visible");})
         .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
         .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+
+    ////////////////////////////////////////////Comparasion//////////////////////////////////////////////////////////////
+
+    //subgroup - plataforms ; group - age
+
+        final_ages = []
+        for (let n of all_platforms["netflix"]){
+            for (let p of all_platforms["prime"]){
+                for(let h of all_platforms["hulu"]){
+                    for(let d of all_platforms["disney"]){
+                        if (n.age == "7+" && p.age =="7+" && h.age == "7+" && d.age=="7+"){
+                            final_ages.push({
+                                "age":"7+",
+                                "netflix" : n.value,
+                                "prime" : p.value,
+                                "hulu" : h.value,
+                                "disney" : d.value
+                            })
+                        }
+                        else if (n.age == "13+" && p.age =="13+" && h.age == "13+" && d.age=="13+"){
+                            final_ages.push({
+                                "age":"13+",
+                                "netflix" : n.value,
+                                "prime" : p.value,
+                                "hulu" : h.value,
+                                "disney" : d.value
+                            })
+                        }
+                        else if (n.age == "16+" && p.age =="16+" && h.age == "16+" && d.age=="16+"){
+                            final_ages.push({
+                                "age":"16+",
+                                "netflix" : n.value,
+                                "prime" : p.value,
+                                "hulu" : h.value,
+                                "disney" : d.value
+                            })
+                        }
+                        else if (n.age == "18+" && p.age =="18+" && h.age == "18+" && d.age=="18+"){
+                            final_ages.push({
+                                "age":"18+",
+                                "netflix" : n.value,
+                                "prime" : p.value,
+                                "hulu" : h.value,
+                                "disney" : d.value
+                            })
+                        }
+                        else if (n.age == "all" && p.age =="all" && h.age == "all" && d.age=="all"){
+                            final_ages.push({
+                                "age":"all",
+                                "netflix" : n.value,
+                                "prime" : p.value,
+                                "hulu" : h.value,
+                                "disney" : d.value
+                            })
+                        }
+                    }
+                }
+            }
+        }
         
+    var container = d3.select('#allComparasion'),
+        width = 1000,
+        height = 520,
+        margin = {top: 20, right: 20, bottom: 40, left: 60},
+
+        barPadding = .2,
+        axisTicks = {qty: 5, outerSize: 0, dateFormat: '%m-%d'};
+    var svg = container
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    var xScale0 = d3.scaleBand().range([0, width - margin.left - margin.right]).padding(barPadding)
+    var xScale1 = d3.scaleBand()
+    var yScale = d3.scaleLinear().range([height - margin.top - margin.bottom, 0])
+
+    var xAxis = d3.axisBottom(xScale0).tickSizeOuter(axisTicks.outerSize);
+    var yAxis = d3.axisLeft(yScale).ticks(axisTicks.qty).tickSizeOuter(axisTicks.outerSize);
+    xScale0.domain(final_ages.map(d => d.age))
+    xScale1.domain(['netflix', 'prime','hulu','disney']).range([0, xScale0.bandwidth()])
+    yScale.domain([0, 3000])
+
+    var model_name = svg.selectAll(".age")
+    .data(final_ages)
+    .enter().append("g")
+    .attr("class", "model_name")
+    .attr("transform", d => `translate(${xScale0(d.age)},0)`);
+
+    var tooltip = d3.select("body")
+    .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 1)
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+
+    /* Add netflix bars */
+    model_name.selectAll(".bar.netflix")
+    .data(d => [d])
+    .enter()
+    .append("rect")
+    .attr("class", "bar netflix")
+    .style("fill","blue")
+    .attr("x", d => xScale1('netflix'))
+    .attr("y", d => yScale(d.netflix))
+    .attr("width", xScale1.bandwidth())
+    .attr("height", d => {
+    return height - margin.top - margin.bottom - yScale(d.netflix)
+    })
+    .on("mouseover", function(d){tooltip.text("Netflix: "+d.netflix); return tooltip.style("visibility", "visible");})
+    .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+    .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+
+    /* Add prime bars */
+    model_name.selectAll(".bar.prime")
+    .data(d => [d])
+    .enter()
+    .append("rect")
+    .attr("class", "bar prime")
+    .style("fill","red")
+    .attr("x", d => xScale1('prime'))
+    .attr("y", d => yScale(d.prime))
+    .attr("width", xScale1.bandwidth())
+    .attr("height", d => {
+    return height - margin.top - margin.bottom - yScale(d.prime)
+    })
+    .on("mouseover", function(d){tooltip.text("Amazon Prime: "+d.prime); return tooltip.style("visibility", "visible");})
+    .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+    .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+
+    /* Add hulu bars */
+    model_name.selectAll(".bar.hulu")
+    .data(d => [d])
+    .enter()
+    .append("rect")
+    .attr("class", "bar hulu")
+    .style("fill","orange")
+    .attr("x", d => xScale1('hulu'))
+    .attr("y", d => yScale(d.hulu))
+    .attr("width", xScale1.bandwidth())
+    .attr("height", d => {
+    return height - margin.top - margin.bottom - yScale(d.hulu)
+    })
+    .on("mouseover", function(d){tooltip.text("Hulu: "+d.hulu); return tooltip.style("visibility", "visible");})
+    .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+    .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+
+    /* Add disney bars */
+    model_name.selectAll(".bar.disney")
+    .data(d => [d])
+    .enter()
+    .append("rect")
+    .attr("class", "bar disney")
+    .style("fill","green")
+    .attr("x", d => xScale1('disney'))
+    .attr("y", d => yScale(d.disney))
+    .attr("width", xScale1.bandwidth())
+    .attr("height", d => {
+    return height - margin.top - margin.bottom - yScale(d.disney)
+    })
+    .on("mouseover", function(d){tooltip.text("Disney: "+d.disney); return tooltip.style("visibility", "visible");})
+    .on("mousemove", function(){return tooltip.style("top", (d3.event.pageY-10)+"px").style("left",(d3.event.pageX+10)+"px");})
+    .on("mouseout", function(){return tooltip.style("visibility", "hidden");});
+
+    // Add the X Axis
+    svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", `translate(0,${height - margin.top - margin.bottom})`)
+    .call(xAxis)
+
+    // Add X axis label:
+    svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("x", width-100)
+    .attr("y", 490)
+    .text("Age group");
+
+    // Add the Y Axis
+    svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+    //Add Y axis label:
+    svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("transform", "rotate(-90)")
+    .attr("y", -margin.left+20)
+    .attr("x", -margin.top)
+    .text("Nº of movies")
+
+
 });
